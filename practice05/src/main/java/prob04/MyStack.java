9a06633b2d325f05372d898bab2e58719b884983
Gimun Lee <wlrn0514@gmail.com>
2024-11-25 08:54:19 +0900

    commit pracitce 05 - 04

--- practice05/src/main/java/prob04/MyStack.java
+++ practice05/src/main/java/prob04/MyStack.java
@@ -1,32 +1,44 @@
 package prob04;
-// resize??   array로 만들어라?
-
-// 배열에 대한 지식 부족... 강사님 코드 보고 공부하기
-// resiz를 어떻게 활용할지 모르겠음
+// resize : 배열 크기 변경
 public class MyStack {
 	private int top;
 	private String[] buffer;
 
 	public MyStack(int capacity) {
-		buffer = new String[capacity];
+		this.buffer = new String[capacity];
+		// 생성자에서 -1인 이유가 뭐지  (구글링)
+		this.top = -1;
 	}
 
 	public void push(String s) {
-		String buffer[];
+		// (구글링)
+		if(top == buffer.length-1) {
+			resize();
+		}
+		
+		top++;
+		buffer[top]=s;
 	}
 
 	// throws 란 예외처리 책임을 떠넘김
+	// throws, throw 다시 공부하기
 	public String pop() throws MyStackException {
-		/* 구현하기 */
-		return null;
+		return buffer[top--];
 	}
 
 	public boolean isEmpty() {
-		/* 구현하기 */
-		return false;
+		// 구글링
+		// top이 -1이라면 수정된게 없다는 의미
+		// boolean이므로 returun 값이 true, false 인지
+		return top == -1;
 	}
-
+	// 다른 함수에서 선언된 배열 길이를 어케 늘려요...
+	// -> 새로운 배열을 만들어라
 	private void resize() {
-		/* 구현하기 */
+		String temp[] = new String[buffer.length*2];
+		for(int i=0; i<top;i++) {
+			temp[i]=buffer[i];
+		}
+		buffer = temp;
 	}	
 }
