39cff2e53d233707d8384b1f992a33677b8dfc43
Gimun Lee <wlrn0514@gmail.com>
2024-12-13 08:35:20 +0900

    practice 04-6

--- practice04/src/main/java/prob06/Money.java
+++ practice04/src/main/java/prob06/Money.java
@@ -27,15 +27,30 @@ public class Money {
 		return new Money(this.amount / money.amount);
 	}
 
+//	@Override
+//	public boolean equals(Object obj) {
+//		// 1. 머니타입 확인
+//      // istanceof 는 서브클래스에서도 true를 반환하기에 상황에 맞게 사용
+//		if (obj instanceof Money) {
+//			// 2. 금액 동일여부 확인
+//          // 값 비교만 한다면 if문을 쓰기보다 return에 boolean 형태로 작성	
+//			if(((Money) obj).amount==this.amount) {
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+	
 	@Override
 	public boolean equals(Object obj) {
-		// 1. 머니타입 확인
-		if (obj instanceof Money) {
-			// 2. 금액 동일여부 확인
-			if(((Money) obj).amount==this.amount) {
-				return true;
-			}
-		}
-		return false;
+		// this가 객체를 지칭
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Money other = (Money) obj;
+		return amount == other.amount;
 	}
 }
